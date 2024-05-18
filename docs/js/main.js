6f6bd725de161be72f5892f3658260d5f95ff1a7
Yash Khasbage <yashkhasbage25@gmail.com>
2024-05-18 21:18:52 +0530

    dynamic sizing of index img

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,15 +4,3 @@ function navOnClickHandler(chapterName) {
     let iframe = document.getElementById(iframeId);
     iframe.src = `./${chapterName}.html`;
 }
-
-window.onload = function() {
-    // Get the image element
-    var img = document.getElementById("image");
-    
-    // Check if the image width exceeds the container width
-    if (window.innerWidth < 700) {
-      // Resize the image to fit the container width
-      img.style.width = "300px";
-      img.style.height = "auto";
-    }
-  };
