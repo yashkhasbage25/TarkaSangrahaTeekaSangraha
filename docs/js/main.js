941cb45c7d07d3c475177a693c397690113f18c4
Yash Khasbage <yashkhasbage25@gmail.com>
2024-05-18 20:58:34 +0530

    resize main page img

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,3 +4,15 @@ function navOnClickHandler(chapterName) {
     let iframe = document.getElementById(iframeId);
     iframe.src = `./${chapterName}.html`;
 }
+
+window.onload = function() {
+    // Get the image element
+    var img = document.getElementById("image");
+    
+    // Check if the image width exceeds the container width
+    if (img.clientWidth > window.innerWidth) {
+      // Resize the image to fit the container width
+      img.style.width = "100%";
+      img.style.height = "auto";
+    }
+  };
