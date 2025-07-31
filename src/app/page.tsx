45d6e64d0eff8465b8ebbc5382fe44aaf5ba2371
Yash Khasbage <yashkhasbage25@gmail.com>
2025-07-31 14:01:56 +0530

    added english text on frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';  
 import Image from "next/image";  
 import Box from "@mui/material/Box";
-import { Eczar, Raleway } from "next/font/google";  
+import { Eczar, Raleway, Zilla_Slab } from "next/font/google";  
 import Stack from '@mui/material/Stack';  
 import Link from "next/link";
   
@@ -14,11 +14,16 @@ const raleway = Raleway({
   weight: "400",  
   subsets: ["latin"],  
 });
+
+const zillaSlab = Zilla_Slab({  
+  weight: "400",  
+  subsets: ["latin"],  
+});
   
 export default function Home() {  
   return (  
     <div className="flex flex-col grow items-center justify-center w-full">  
-      <Stack spacing={8} className="flex flex-col items-center justify-center py-8 px-4 w-full sm:w-4/5 md:w-4/5 max-w-screen-lg">  
+      <Stack spacing={8} className="flex flex-col items-center justify-center py-16 px-4 w-full sm:w-4/5 md:w-4/5 max-w-screen-lg">  
         <Box className={`text-4xl text-center ${eczar.className} break-all hyphens-auto`}>  
           ॥ नमः कणादगौतमवात्स्यायनादिभ्यो न्यायशास्त्रसम्प्रदायकर्तृभ्यो <br />  
           वंशर्षिभ्यः परमऋषिभ्यो महद्भ्यो मुनिभ्यो गुरुभ्यः॥  
@@ -30,14 +35,21 @@ export default function Home() {
           alt="Lord Ganesha"  
           className="rounded-full shadow-lg"  
         />  
-        <Box className={`text-2xl text-center ${eczar.className}`}>  
+        <Box className={`text-3xl text-center ${eczar.className}`}>  
           प्रमाणविषयी वादः पदार्थस्थापकस्तर्कः। <br />  
           स्फूर्यते भजनाद्यस्य तं गणेशं नमाम्यहम्॥ <br />  
         </Box>  
+
+        <Box className={`text-3xl w-full text-center ${eczar.className} py-12`}>  
+          TarkaSangraha.com - A single stop solution to entire TarkaSangraha.
+          Read Tīkās of Deepika, Nyayabodhini, and many more on a single website. 
+        </Box>  
+
         {/* Create a box with transparent background saying 'Recent updates' */}
-        <Box className={`text-md text-left ${raleway.className} w-full bg-transparent border border-gray-300 rounded-lg p-4`}> 
-          Recent Updates:
-          <ul className="list-disc list-inside">
+        <div className={`text-xl text-left ${zillaSlab.className} w-full bg-transparent p-8 border-t border-b border-gray-300 space-y-2 mt-16 text-gray-300`}> 
+          Recent Updates: 
+
+          <ul className="list-disc list-inside space-y-4">
             <li>The entire text of TarkaSangraha has been uploaded. To view this go, ग्रन्थाः -- Select Books to Read -- Select तर्कसङ्ग्रहः -- Click Read.</li>
             <li>Similarly, Teekas of Nyayabodhini, TarkaSangrahaDeepika, TarkaSangrahaSarvasvam, and Aaloka have been uploaded</li>
             <li>Search capability in TarkaSamgraha text. To try this go, अन्वेषणम् -- enter keyword -- select book as Tarkasangraha -- Click Search. You can also &quot;Open the section in new tab&quot;!</li>
@@ -45,13 +57,13 @@ export default function Home() {
           </ul>
           <b>Website is under intial stages of development.</b> For feature requests, contribution of texts, or new ideas contact: yashkhasbage25@gmail.com <br /> 
           <Link href="/planned-features" className="text-blue-500 hover:underline">Planned Features</Link>
-        </Box>
+        </div>
 
       </Stack>  
       <footer className="w-full text-white py-8 px-4 mt-8">  
         <div className="max-w-screen-lg mx-auto flex flex-col sm:flex-row justify-between items-center">  
           <div className="mb-4 sm:mb-0">  
-            <Box className={`text-center sm:text-left ${raleway.className}`}>  
+            <Box className={`text-center sm:text-left ${zillaSlab.className}`}>  
               © {new Date().getFullYear()} Tarka Sangraha. All rights reserved.  
             </Box>  
           </div>  
